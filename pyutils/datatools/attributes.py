00640b58a5c92dcbfa1f41dde7ae76401138781c
Tudor Avram <a96tudor@gmail.com>
2024-12-10 21:43:21 +0200

    Add more general methods to AttributesCollection

--- pyutils/datatools/attributes.py
+++ pyutils/datatools/attributes.py
@@ -1,4 +1,6 @@
-from typing import Iterable, List, Optional
+from typing import Any, Iterable, List, Optional
+
+from typing_extensions import SupportsIndex
 
 from pyutils.datatools.collection import Collection
 
@@ -40,13 +42,20 @@ class Attribute:
     def from_dict(cls, data: dict) -> "Attribute":
         return cls(data["name"], data["value"])
 
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Attribute):
+            return False
+
+        return self.name == other.name and self.value == other.value
+
 
 class AttributesCollection(Collection):
-    __map: dict = {}
+    __map: dict
 
     def __init__(self, values: Optional[Iterable[Attribute]] = None):
         super().__init__(values)
 
+        self.__map = {}
         if values:
             self.__map = {attribute.name: attribute for attribute in values}
 
@@ -69,3 +78,49 @@ class AttributesCollection(Collection):
     @classmethod
     def from_dicts(cls, data: List[dict]) -> "AttributesCollection":
         return cls([Attribute.from_dict(d) for d in data])
+
+    def append(self, __object: Any):
+        if not isinstance(__object, Attribute):
+            raise ValueError(
+                "Only Attribute objects can be appended to an AttributesCollection"
+            )
+
+        super().append(__object)
+        self.__map[__object.name] = __object
+
+    def extend(self, __iterable: Iterable):
+        if any(not isinstance(item, Attribute) for item in __iterable):
+            raise ValueError(
+                "Only lists of Attribute objects can be used "
+                "to extend an AttributesCollection"
+            )
+
+        super().extend(__iterable)
+
+        for item in __iterable:
+            self.__map[item.name] = item
+
+    def insert(self, __index: SupportsIndex, __object: Any):
+        if not isinstance(__object, Attribute):
+            raise ValueError(
+                "Only Attribute objects can be inserted into an AttributesCollection"
+            )
+        super().insert(__index, __object)
+        self.__map[__object.name] = __object
+
+    def clone(self, read_only: bool = False) -> "Collection":
+        result = AttributesCollection(self)
+        result.read_only = read_only
+
+        return result
+
+    def delete(self, __index: SupportsIndex):
+        attribute = self[__index]
+        super().delete(__index)
+        del self.__map[attribute.name]
+
+    def pop(self, __index: Optional[SupportsIndex] = None) -> Any:
+        result = super().pop(__index)
+        del self.__map[result.name]
+
+        return result
