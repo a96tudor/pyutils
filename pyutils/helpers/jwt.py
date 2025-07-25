db65bb9bc5d4ac4f506b60d2be1aae6931e00d59
Tudor Avram <a96tudor@gmail.com>
2025-07-25 23:38:08 +0200

    Add JWT encoding and decoding functionalities

---
+++ pyutils/helpers/jwt.py
@@ -0,0 +1,19 @@
+import datetime
+from typing import Optional
+
+import jwt
+
+def get_jwt_token(
+    payload: dict,
+    key: str,
+    ttl: Optional[datetime.timedelta] = datetime.timedelta(hours=1)
+) -> str:
+    payload['exp'] = (datetime.datetime.now(datetime.timezone.utc) + ttl).timestamp()
+    return jwt.encode(payload, key=key, algorithm='HS256')
+
+
+def decode_token(token: str, key: str) -> dict:
+    try:
+        return jwt.decode(token, key=key, algorithms=['HS256'])
+    except Exception as e:
+        print(e)
